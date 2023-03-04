efeb4eb25966c28537782f53dbffc2f09948da5e
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-03-04 09:32:19 -0600

    se agregaron los DbSet al Context de la base de datos

--- L01_2020MS650/Models/restauranteContext.cs
+++ L01_2020MS650/Models/restauranteContext.cs
@@ -10,6 +10,12 @@ namespace L01_2020MS650.Models
             
         }
 
-        public DbSet<restauranteDB> restauranteDB { get; set; }
+        public DbSet<clientes> clientes { get; set; }
+
+        public DbSet<motorista> motorista { get; set; }
+
+        public DbSet<pedidos> pedidos { get; set; }
+
+        public DbSet<platos> platos { get; set; }
     }
 }
