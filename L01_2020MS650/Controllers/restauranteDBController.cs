efde41bd5d8515b6e004a3198d86962d19e71dbe
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-03-04 09:38:31 -0600

    Creación del controlador del resturante

---
+++ L01_2020MS650/Controllers/restauranteDBController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using L01_2020MS650.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace L01_2020MS650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class restauranteDBController : ControllerBase
+    {
+
+        private readonly restauranteContext _restauranteDBContexto;
+
+        public restauranteDBController(restauranteContext equiposContexto)
+        {
+            _restauranteDBContexto = equiposContexto;
+        }
+
+    }
+}
